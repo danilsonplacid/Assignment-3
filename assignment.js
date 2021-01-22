b02fac0e819bf59f1cc1b10841a90de094f9df5f
danilsonplacid <denilsonplacid@gmail.com>
2021-01-22 23:46:32 +0600

    assignment

---
+++ assignment.js
@@ -0,0 +1,83 @@
+
+
+
+// kilometerToMeter
+
+
+function kilometerToMeter(convertMeter){
+    var meter = convertMeter*1000;
+    return meter;
+}
+
+
+
+
+
+// budgetCalculator 
+
+
+function budgetCalculator(watch,mobile,laptop){
+    var watchPrice = 50;
+    var mobilePrice = 100;
+    var laptopPrice = 500;
+
+    var watch = watchPrice*3;
+    var mobile = mobilePrice*5;
+    var laptop = laptopPrice*10;
+    total = watch + mobile + laptop;
+    return total;
+}
+
+
+
+//  hotelCost 
+
+
+function hotelCost (day){
+    var costAmount = 0;
+    if (day <=10) {
+        costAmount = days*100;
+    }
+    else if (day <=20){
+        var firstTenDays = 10 * 100;
+        var remaining = day - 10;
+        var secondTenDays = remaining * 80;
+        hotelCost = firstTenDays + secondTenDays;
+
+    }
+    else {
+        var firstTenDays = 10*100;
+        var secondTenDays = 10*80;
+        var remaining = day - 20;
+        var thirdTendays = remaining *50;
+        hotelCost = firstTenDays + secondTenDays + thirdTendays;
+
+    }
+    return hotelCost;
+}
+
+
+
+
+
+
+
+
+// megaFriend 
+
+    function megaFriend(friendNames) {
+        var max = friendNames[0];
+        for (var i = 0; i < friendNames.length; i++) {
+            var element = friendNames[i];
+            if (max.length < element.length) {
+                max = element;
+            }
+        }
+        return max;
+    }
+    
+    
+    
+
+
+
